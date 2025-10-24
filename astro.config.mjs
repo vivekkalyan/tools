26d002969dc9757705f1ace979e3f2233acdc985
Vivek Kalyan <hello@vivekkalyan.com>
2025-10-25 00:39:44 +0800

    chore: Setup react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 
+import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
@@ -8,4 +9,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [react()],
 });
